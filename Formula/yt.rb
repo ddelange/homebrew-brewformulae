5159b0200344ce3a2fa79a01bd16715eec841b4d
david <14880945+ddelange@users.noreply.github.com>
2020-09-11 21:18:36 +0200

    Update yt.rb

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -5,16 +5,15 @@ class Yt < Formula
   url "https://github.com/ddelange/yt/archive/0.2.0.tar.gz"
   sha256 "28429cc416582558a5be3607345dd05fe67ba25fee5e46ebbe765d0d9ea7342d"
 
-  version "0.2.0"
-  # revision 1
+  revision 1
   head "https://github.com/ddelange/yt.git"
 
   bottle :unneeded
 
   depends_on "homebrew-ffmpeg/ffmpeg/ffmpeg" => ["with-fdk-aac", "with-srt", "with-wavpack", "with-xvid"]
-  depends_on "youtube-dl"
   depends_on "atomicparsley"
-
+  depends_on "youtube-dl"
+  
   def install
     system "bash", "./install.sh", "--prefix=#{prefix}", "yt"
   end
