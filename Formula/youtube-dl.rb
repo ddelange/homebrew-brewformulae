f14a817f117a690253430f40f8839272ba3da4d7
ddelange <14880945+ddelange@users.noreply.github.com>
2022-09-27 10:52:02 +0200

    Add --rm-cache-dir to youtube.dl.rb

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -13,6 +13,7 @@ class YoutubeDl < Formula
     man1.install_symlink libexec/"share/man/man1/youtube-dl.1" => "youtube-dl.1"
     bash_completion.install libexec/"etc/bash_completion.d/youtube-dl.bash-completion"
     fish_completion.install libexec/"etc/fish/completions/youtube-dl.fish"
+    system "#{bin}/youtube-dl", "--rm-cache-dir"  # https://github.com/ytdl-org/youtube-dl/issues/31164#issuecomment-1259189511
   end
 
   test do
