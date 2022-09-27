8d38c2638383b1ec34849b10bcdfb19f02ef5f31
ddelange <14880945+ddelange@users.noreply.github.com>
2022-09-27 10:47:35 +0200

    Point yt.rb to head-only youtube-dl formula

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -4,12 +4,13 @@ class Yt < Formula
   homepage "https://github.com/ddelange/yt"
   url "https://github.com/ddelange/yt/archive/0.3.1.tar.gz"
   sha256 "5094706a6d5047366f1e9906601200b1d447dc5e537a3a05c50669a2914a8e77"
+  revision 1
 
   head "https://github.com/ddelange/yt.git"
 
   depends_on "atomicparsley"
   depends_on "homebrew-ffmpeg/ffmpeg/ffmpeg" => ["with-fdk-aac", "with-srt", "with-wavpack", "with-xvid"]
-  depends_on "youtube-dl"
+  depends_on "ddelange/brewformulae/youtube-dl"
 
   def install
     system "bash", "./install.sh", "--prefix=#{prefix}", "yt"
