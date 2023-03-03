451a343d28958d16f14d68d974cf27576e975e00
ddelange <14880945+ddelange@users.noreply.github.com>
2023-03-03 09:11:52 +0100

    yt 0.4.1 (#18)
    Co-authored-by: ddelange <ddelange@users.noreply.github.com>

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -2,8 +2,8 @@ class Yt < Formula
   desc "Fine-tuning the use of youtube-dl for audio and videophiles"
 
   homepage "https://github.com/ddelange/yt"
-  url "https://github.com/ddelange/yt/archive/0.4.0.tar.gz"
-  sha256 "5bfda40960462479600da3a1ce622d688d4edc3888cfe085c5c4092f841dcc02"
+  url "https://github.com/ddelange/yt/archive/0.4.1.tar.gz"
+  sha256 "30b0a3ca343044372903b4db12deb97b3f64c10958dff35d60962449ca814fe1"
   head "https://github.com/ddelange/yt.git"
 
   depends_on "atomicparsley"
