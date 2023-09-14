2b9fd68f7fcbc516437bddda898ce6ceb37c39de
ddelange <ddelange@users.noreply.github.com>
2023-09-14 10:44:09 +0200

    Prepare for yt-dlp

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -1,5 +1,5 @@
 class Yt < Formula
-  desc "Fine-tuning the use of youtube-dl for audio and videophiles"
+  desc "Fine-tuning the use of youtube-dl / yt-dlp for audio and videophiles"
 
   homepage "https://github.com/ddelange/yt"
   url "https://github.com/ddelange/yt/archive/0.4.3.tar.gz"
@@ -7,8 +7,8 @@ class Yt < Formula
   head "https://github.com/ddelange/yt.git"
 
   depends_on "atomicparsley"
-  depends_on "homebrew-ffmpeg/ffmpeg/ffmpeg" => ["with-fdk-aac", "with-srt", "with-wavpack", "with-xvid"]
-  depends_on "ddelange/brewformulae/youtube-dl"
+  depends_on "homebrew-ffmpeg/ffmpeg/ffmpeg" => ["with-fdk-aac", "with-srt", "with-wavpack", "with-xvid", "enable-audiotoolbox"]
+  depends_on "yt-dlp"
 
   def install
     system "bash", "./install.sh", "--prefix=#{prefix}", "yt"
