6a0f2e1384aa8bbc0894a2e251cdf7e32359df5c
ddelange <14880945+ddelange@users.noreply.github.com>
2023-12-28 12:46:32 +0100

    Replace spek appcast with livecheck

--- Casks/spek.rb
+++ Casks/spek.rb
@@ -3,10 +3,14 @@ cask "spek" do
   sha256 "6426c4c34ac8bcb2fa1020cb295e3c105ca912c853cbbe84d1d196a32bba361b"
 
   url "https://github.com/withmorten/spek-alternative/releases/download/#{version}/spek-alternative-#{version}.dmg"
-  appcast "https://github.com/withmorten/spek-alternative/releases.atom"
   name "Spek"
   desc "Acoustic spectrum analyzer"
   homepage "https://github.com/withmorten/spek-alternative"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "Spek.app"
 
